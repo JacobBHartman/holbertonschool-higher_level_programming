048093122cffe8e9965ab39c17ee2858d79a976f
jacobbhartman <209@holbertonschool.com>
2017-12-06 02:18:51 +0000

    did the c portion, lets see what the checker thinks

---
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+
+/**
+ * insert_node - inserts a number into a sorted signly linked list
+ * @head: a pointer to a pointer to a list
+ * @number: the number to be inserted
+ *
+ * Return: the address of the new node, or NULL if it failed
+ */
+listint_t *insert_node(listint_t **head, int number)
+{
+	listint_t *temp = *head;
+	listint_t *new_node;
+	listint_t *breadcrumb;
+
+	new_node = malloc(sizeof(listint_t));
+	if (new_node == NULL)
+		return (NULL);
+	new_node->n = number;
+	new_node->next = NULL;
+
+	while (number > temp->next->n)
+		temp = temp->next;
+	breadcrumb = temp;
+	temp = temp->next;
+	breadcrumb->next = new_node;
+	new_node->next = temp;
+
+	return(breadcrumb->next);
+}
