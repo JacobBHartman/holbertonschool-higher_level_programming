0db59ef3d7c19ee38a981f00c249e706f2b5c64c
JacobBHartman <209@holbertonschool.com>
2017-12-07 21:29:44 +0000

    Secondary commit, ready to look at checker

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import hidden_4
+    dir_len = len(dir(hidden_4))
+    for i in range(dir_len):
+        if dir(hidden_4)[i][0:2] != "__":
+           print("{:s}".format(dir(hidden_4)[i]))
