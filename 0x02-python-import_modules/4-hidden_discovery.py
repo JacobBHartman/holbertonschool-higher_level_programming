94e092fbff46ede755862d22fd930d443c48d0b6
JacobBHartman <209@holbertonschool.com>
2017-12-07 21:32:28 +0000

    fixed indentation

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -4,4 +4,4 @@ if __name__ == "__main__":
     dir_len = len(dir(hidden_4))
     for i in range(dir_len):
         if dir(hidden_4)[i][0:2] != "__":
-           print("{:s}".format(dir(hidden_4)[i]))
+            print("{:s}".format(dir(hidden_4)[i]))
