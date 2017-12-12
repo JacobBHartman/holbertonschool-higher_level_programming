1ab3ad6572060d8834a086d429d4378a76afdd8b
JacobBHartman <209@holbertonschool.com>
2017-12-12 22:08:37 +0000

    now properly modifying a string

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,5 +1,12 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    new_string = my_string.replace('C', '')
-    new_string = new_string.replace('c', '')
-    return new_string
+    new_string = list(my_string)
+    lng = len(new_string)
+    i = 0
+    while i < lng:
+        if new_string[i] is 'c' or new_string[i] is 'C':
+            del new_string[i]
+            lng -= 1
+        i += 1
+    new_list = ''.join(new_string)
+    return new_list
