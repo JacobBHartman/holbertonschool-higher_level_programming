cca97170530ee0a689b71e614f7818fe57c8700c
JacobBHartman <209@holbertonschool.com>
2017-12-12 23:53:05 +0000

    wasnt passing, probably because im adding tuples larger than 2

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -2,9 +2,9 @@
 def add_tuple(tuple_a=(), tuple_b=()):
     nt = [0, 0]
 
-    for i in range(len(tuple_a)):
+    for i in range(min(len(tuple_a), 2)):
         nt[i] += tuple_a[i]
-    for i in range(len(tuple_b)):
+    for i in range(min(len(tuple_b), 2)):
         nt[i] += tuple_b[i]
 
     return (nt[0], nt[1])
