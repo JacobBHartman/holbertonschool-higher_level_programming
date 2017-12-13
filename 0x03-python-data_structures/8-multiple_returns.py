1d5a7cb515a234db106c1d41dd30424cda8d4f12
JacobBHartman <209@holbertonschool.com>
2017-12-13 00:02:14 +0000

    now properly accounting for the stated edge case

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    tuple = ["", None]
-    if sentence is None:
-        return None
+    tuple = [0, '']
+    if sentence is "":
+        return (0, None)
     tuple[1] = sentence[0]
     tuple[0] = len(sentence)
     return (tuple[0], tuple[1])
