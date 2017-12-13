49236ec13634c6c1aea2ad401465fb36d3dbf0c3
JacobBHartman <209@holbertonschool.com>
2017-12-13 02:29:18 +0000

    i just whiteboarded, now im confident, about to pass all the checks first try!

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,43 @@
+#include "lists.h"
+
+/**
+ * is_palindrome - checks if a singly linked list is a palindrome
+ * @head: a pointer to a pointer to a singly linked list
+ * Return: 0 if not a palindrome, 1 if is a palindrome
+ */
+int is_palindrome(listint_t **head)
+{
+	/* declare variables */
+	listint_t *top = *head;
+	listint_t *bot = *head;
+	listint_t *crumb = *head;
+
+	/* check if head is null */
+	if (head == NULL)
+		return (0);
+
+	if (*head == NULL)
+		return (1);
+
+	/* initialize the positions of the bot pointer and crumb */
+	while (bot->next != NULL)
+	{
+		if (bot->next->next == NULL)
+			crumb = bot;
+		bot = bot->next;
+	}
+
+	/* primary functionality */
+	while (bot != top && bot->next != top)
+	{
+		if (bot->n != top->n)
+			return (0);
+		bot = crumb;
+		top = top->next;
+		crumb = top;
+		while (crumb->next != bot && bot->next != crumb)
+			crumb = crumb->next;
+	}
+	return (1);
+
+}
