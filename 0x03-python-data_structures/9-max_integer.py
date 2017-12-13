de148a02755f229fb73e74ea0d8987569235e6c6
JacobBHartman <209@holbertonschool.com>
2017-12-13 00:17:19 +0000

    trying to account for empty list properly

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    if my_list is []:
+    if my_list is None:
         return None
-    max = 0
+    max = my_list[0]
     for i in range(len(my_list)):
         if my_list[i] > max:
             max = my_list[i]
