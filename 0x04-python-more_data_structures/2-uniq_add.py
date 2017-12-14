21ef0f1479d9b650f3d83b7bb7dfc37df4ae72bd
JacobBHartman <209@holbertonschool.com>
2017-12-14 23:11:29 +0000

    initial commit for task

---
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def uniq_add(my_list=[]):
+    my_set = set(my_list)
+    my_list = list(my_set)
+    result = 0
+    for i in range(len(my_list)):
+        result += my_list[i]
+    return result
