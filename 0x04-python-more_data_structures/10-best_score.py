d6611ddb16faf85ebb58983b78b1543c1bfb31b2
JacobBHartman <209@holbertonschool.com>
2017-12-15 02:43:16 +0000

    trying to not return an empty string

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 def best_score(my_dict):
-    if my_dict is None:
-        return None
-    largest = 0
-    name = ""
-    new_dict = dict(my_dict)
-    for i, j in new_dict.items():
-        if j > largest:
-            largest = j
-            name = i
-    return name
+    if my_dict:
+        largest = 0
+        name = ""
+        new_dict = dict(my_dict)
+        for i, j in new_dict.items():
+            if j > largest:
+                largest = j
+                name = i
+        return name
