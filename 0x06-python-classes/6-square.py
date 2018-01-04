2c29c682a8b21903fe6fc56660ec25b7ca667b3a
JacobBHartman <209@holbertonschool.com>
2018-01-04 02:09:46 +0000

    what

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-class Square():
+class Square:
 
     """This is an empty class that defines a square"""
 
@@ -65,10 +65,11 @@ class Square():
         """
         if self.__size == 0:
             print()
+            return
         else:
             for i in range(self.__position[1]):
                 print()
-            for i in range(self.__size):
+            for l in range(self.__size):
                 for k in range(self.__position[0]):
                     print(" ", end="")
                 for j in range(self.__size):
