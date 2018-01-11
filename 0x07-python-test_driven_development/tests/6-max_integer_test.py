aa7d5cedc9da1844ee72acf720c524ed9988c884
jacobbhartman <209@holbertonschool.com>
2018-01-11 02:55:18 +0000

    now testing 1-item list

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -16,6 +16,7 @@ class TestMaxInteger(unittest.TestCase):
         self.assertEqual(max_integer([1, 2, 3, 4, 5]), 5)
         self.assertEqual(max_integer([1, 2, 5, 3, 4]), 5)
         self.assertEqual(max_integer([5, 1, 2, 3, 4]), 5)
+        self.assertEqual(max_integer([5]), 5)
 
     def test_list_empty(self):
         """Testing if list is empty"""
