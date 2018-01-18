0d81fe3bc3c0752f52786cd98f04d373da8c661a
jacobbhartman <209@holbertonschool.com>
2018-01-18 04:43:23 +0000

    initial commit for task

---
+++ 0x0B-python-input_output/10-class_to_json.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+    contains a function that returns the dictionary description with simple data
+    structure (list, dictionary, string, integer and boolean) for JSON
+    serialization of an object
+"""
+
+
+import json
+
+
+def class_to_json(obj):
+    """
+        a function described by the module
+    """
+    return(vars(obj))
