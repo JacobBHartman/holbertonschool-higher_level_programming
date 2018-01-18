8d6569d0890e0e3f14f875a558b670fd6a580ee3
jacobbhartman <209@holbertonschool.com>
2018-01-18 01:30:12 +0000

    change specific file name to generalized variable called filename

--- 0x0B-python-input_output/1-number_of_lines.py
+++ 0x0B-python-input_output/1-number_of_lines.py
@@ -8,7 +8,7 @@ def number_of_lines(filename=""):
         this function returns the number of lines of a text file
     """
     number_of_lines = 0
-    with open("my_file_0.txt", encoding='utf-8') as f:
+    with open(filename, encoding='utf-8') as f:
         for line in f:
             number_of_lines += 1
     return number_of_lines
