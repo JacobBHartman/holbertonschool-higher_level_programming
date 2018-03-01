0fd7aff55138ec96f6986bed43365eafe6e98f59
JacobBHartman <209@holbertonschool.com>
2018-03-01 02:48:12 +0000

    create database, use it, then create table

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,6 +1,8 @@
 -- create a table on a MySQL server
-CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS cities (
 id INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY,
 state_id INT NOT NULL,
-FOREIGN KEY sid_fk(state_id) REFERENCES states(id),
-name VARCHAR(256) NOT NULL);
+name VARCHAR(256) NOT NULL,
+FOREIGN KEY _fk_sid(state_id) REFERENCES states(id) );
