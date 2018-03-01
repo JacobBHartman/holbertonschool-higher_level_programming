048a25d3a1dd9a59b1066ad131c7fc66e8a8d4c9
jacobbhartman <209@holbertonschool.com>
2018-03-01 22:27:43 +0000

    initial commit for task

---
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,7 @@
+-- list all genres from a database and display the number of shows
+SELECT tv_genres.name AS genre, COUNT(genre_id) AS number_shows
+FROM tv_show_genres
+LEFT JOIN tv_genres
+ON tv_show_genres.genre_id = tv_genres.id
+GROUP BY genre
+ORDER BY number_shows DESC;
