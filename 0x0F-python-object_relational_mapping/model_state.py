592706d9294fdb0a219a488180872a9bc26953b6
jacobbhartman <209@holbertonschool.com>
2018-03-24 01:51:16 +0000

    mistype autoincrement, initial commit for task

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -12,5 +12,5 @@ Base = declarative_base()
 
 class State(Base):
     __tablename__ = 'states'
-    id = Column(Integer, primary_key=True, autonincrement=True)
+    id = Column(Integer, primary_key=True, autoincrement=True)
     name = Column(String(128), nullable=False)
