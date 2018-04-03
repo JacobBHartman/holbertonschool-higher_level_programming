c02fb2efb58d5112049843d32f3e4e0f1652792d
JacobBHartman <209@holbertonschool.com>
2018-04-03 14:48:40 -0700

    initial commit for project

---
+++ 0x10-python-network_0/0-body_size.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+# A Bash script that takes in a URL, sends a request to that URL and displays
+# the size of the body of the response
+curl -I $1 | grep -e 'Content-Length' | cut -d: -f2 | cut -d' ' -f2
