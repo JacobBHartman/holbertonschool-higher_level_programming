576c76a26aefe0bda7055a3e90c6199bf6206e3a
JacobBHartman <209@holbertonschool.com>
2018-04-09 12:55:57 -0700

    initial commit for task

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+'''
+    This module contains a Python script that takes in a URL, sends
+    a request to the URL and displays the value of a particular
+    variable
+'''
+
+if __name__ == '__main__':
+    import requests
+    from sys import argv
+    
+    url = argv[1]
+    variable = 'X-Request-Id'
+    content = requests.get(url)
+    
+    print("{}".format(content.headers[variable]))
