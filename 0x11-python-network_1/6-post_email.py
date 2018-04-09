73762cea0060242da33f36b85e1b2e8ec582615a
JacobBHartman <209@holbertonschool.com>
2018-04-09 13:22:41 -0700

    test in container

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+'''
+    This Python script takes in a URL and an eamil, sends a POST
+    request to the passed URL with the email as a parameter, and
+    displays the body of the response
+'''
+
+
+if __name__ == '__main__':
+    import requests
+    from sys import argv
+
+    url = argv[1]
+    email = argv[2]
+
+    values = {'email': email}
+    r = requests.post(url, data=values)
+    print("Your email is: {}".format(r))
