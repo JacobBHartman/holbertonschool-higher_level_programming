fd776510c62d11eeae3c77fcf449a5f5dd95ea1e
JacobBHartman <209@holbertonschool.com>
2018-05-01 14:17:29 -0700

    fix conditions

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 
-if (process.argv[2] && process.argv[3] == null) {
+if (typeof process.argv[2] === 'undefined') {
   console.log('No argument')
-} else if (process.argv[3] && process.argv[4] === 'undefined') {
+} else if (typeof process.argv[3] === 'undefined') {
   console.log('Argument found')
 } else {
   console.log('Arguments found')
