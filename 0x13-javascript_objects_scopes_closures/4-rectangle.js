304c4e82b48ab04eaf2eb6dbcba8cb511715846a
JacobBHartman <209@holbertonschool.com>
2018-05-07 14:48:38 -0700

    initial commit for task

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,29 @@
+#!/usr/bin/node
+
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      let rowOutput = '';
+      for (let j = 0; j < this.width; j++) {
+        rowOutput += 'X';
+      }
+      console.log(rowOutput);
+    }
+  }
+  rotate () {
+    let temp = this.width;
+    this.width = this.height;
+    this.height = temp;
+  }
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
+}
+module.exports = Rectangle;
