acfa11fefa5779db62410af591b261584a66fa4e
JacobBHartman <209@holbertonschool.com>
2018-05-07 15:11:53 -0700

    add semicolon

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -5,7 +5,7 @@ const Rectangle = require('./5-square.js');
 class Square extends Rectangle {
   charPrint (c) {
     if (c === undefined) {
-      c = 'X'
+      c = 'X';
     }
     for (let i = 0; i < this.height; i++) {
       let rowOutput = '';
