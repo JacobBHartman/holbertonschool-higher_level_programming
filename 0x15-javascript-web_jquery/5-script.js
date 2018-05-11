a87a4a9652acc9573b7547f75e2763a373d7b36b
JacobBHartman <209@holbertonschool.com>
2018-05-10 22:45:21 -0700

    commit for project

---
+++ 0x15-javascript-web_jquery/5-script.js
@@ -0,0 +1,4 @@
+$("#add_item").click(function() {
+  let item = "<li>Item</li>";
+  $('.my_list').append(item);
+});
---
+++ 0x15-javascript-web_jquery/6-script.js
@@ -0,0 +1,4 @@
+$("#update_header").click(function() {
+  let replacer = "New Header!!!";
+  $('header').text(replacer);
+});
