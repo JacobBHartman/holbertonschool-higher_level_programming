2fd7a65d00e8f32d1dc8d46e2d00e3cc72ba3577
Dave Yoon <daveyoon64@gmail.com>
2018-10-05 15:24:22 -0700

    this was a demonstration commit

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,5 +1,13 @@
 #!/usr/bin/python3
+'''
+    This Python Module contains a method called 'fizzbuzz' that will
+    print "Fizz" for multiples of 3, "Buzz for multiples of 5
+'''
+
 def fizzbuzz():
+    '''
+        Well here is the method        
+    '''
     for i in range(1, 101):
         if i % 15 == 0:
             print("FizzBuzz ", end="")
